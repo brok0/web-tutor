c32d7c83b11d77bd3d4151620c668c7475af99ce
Boris Hlynsky <86774452+bhlynsky@users.noreply.github.com>
2022-07-07 13:42:20 +0300

    Added cancel button to createTutor form

--- pages/tutor/createTutor.js
+++ pages/tutor/createTutor.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { specializations } from "../../services/constants";
-
+import Link from "next/link";
 export default function CreateTutor() {
   const options = Object.values(specializations);
 
@@ -72,9 +72,10 @@ export default function CreateTutor() {
             <InputField type="number" label="Price" name="price"></InputField>
 
             <div className="flex justify-center mt-5">
-              <button type="submit" className="border rounded p-1 font-semibold w-1/3 m-auto">
-                Cancel
+              <button type="button" className="border rounded p-1 font-semibold w-1/3 m-auto block">
+                <Link href="/search">Cancel</Link>
               </button>
+
               <button type="submit" disabled={isSubmitting} className="border rounded bg-purple-400 p-1 text-white font-semibold w-1/3 m-auto">
                 Submit
               </button>
