fd227f31936584be8fe39139ed28137812f17f7c
brok0 <bglinskii6@gmail.com>
2022-07-07 14:10:18 +0300

    Update CreateLessonModal.js

--- pages/search/components/CreateLessonModal.js
+++ pages/search/components/CreateLessonModal.js
@@ -2,7 +2,7 @@ import Calendar from "react-calendar";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-calendar/dist/Calendar.css";
-
+import { useSelector } from "react-redux";
 //idea : divide modal into pages:
 //	- Pick day page : Today,tommorow etc
 // 	- Pick Time page
@@ -13,7 +13,7 @@ export default function ({ onClose, tutor }) {
   const [date, setDate] = useState(new Date());
   const [dateConfirmed, setDateConfirmed] = useState(false);
   const awailableTime = ["17:00", "18:00", "20:00"]; // should request server after picking date to have these
-  const { data: session, status } = useSession();
+  //const { data: session, status } = useSession();
   //constructing service url
   const baseUrl = useSelector((state) => state.user.baseServiceUrl);
   const requestUrl = baseUrl + "/lesson/createLesson";
@@ -51,7 +51,7 @@ export default function ({ onClose, tutor }) {
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="mt-3 text-center sm:mt-0  sm:text-left h-96 w-full">
               <h3 className="text-lg leading-6 font-medium text-gray-900 border-b-2 mb-2" id="modal-title">
-                Buy Lesson in {specialization} for {pricePerLesson}$
+                Buy Lesson in {specialization} for <b>{pricePerLesson}$</b>
               </h3>
               <div className="m-1">
                 {!dateConfirmed ? (
@@ -60,13 +60,12 @@ export default function ({ onClose, tutor }) {
                     <Calendar value={date} onChange={setDate} minDate={new Date()} className="m-auto " />
                   </div>
                 ) : (
-                  // TODO: Add options chip like : today,tommorow, ...
-                  <div className="">
+                  <div className="mt-16">
                     <h3 className="font-semibold mb-4 text-center">Tutor available at:</h3>
                     <div className="flex items-center justify-center">
                       {awailableTime.map((time) => (
                         <div className="border bg-purple-300 rounded-full flex p-2 hover:bg-purple-500 cursor-pointer">
-                          <p className="m-auto ">{time}</p>
+                          <p className="m-auto font-semibold">{time}</p>
                         </div>
                       ))}
                     </div>
