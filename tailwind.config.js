f906c2c43066ae152906dd7ce6218f927f4234f5
brok0 <bglinskii6@gmail.com>
2022-07-11 17:06:35 +0300

    Update messenger size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			height: {
-				"(msngr-height)": "calc(100vh - 8rem)",
-			},
-			width: {
-				"(msngr-width)": "calc(100vw - 20rem)",
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        "(msngr-height)": "calc(100vh - 12rem)",
+      },
+      width: {
+        "(msngr-width)": "calc(100vw - 22rem)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
