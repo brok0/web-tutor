37d7c8420eec8f167e949122211f7b2486633d82
brok0 <bglinskii6@gmail.com>
2022-07-29 17:28:10 +0300

    update profile page

--- components/ProfilePageNavigation.js
+++ components/ProfilePageNavigation.js
@@ -1,41 +1,21 @@
 import Link from "next/dist/client/link";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
-import ProfileOptions from "../pages/profile/components/ProfileOptions";
 
 export default function ProfilePageNavigation() {
   const { data: session, status } = useSession();
-  const [open, setOpen] = useState(false);
-
-  const toggleOpen = () => {
-    setOpen(!open);
-  };
 
   return (
     <div className="relative bg-purple-500">
-      <div className="flex flex-col items-center border-b-2 px-6 md:flex-row md:justify-start md:space-x-10 text-white">
+      <div className="flex flex-col items-center border-b-2 px-6 h-16 md:flex-row md:justify-start md:space-x-10 text-white">
         <Link href="/profile/messenger" className="inline-block">
           Messages
         </Link>
         <Link href="/profile/lessons" className="inline-block">
           Lessons
         </Link>
-
-        <Link href="" className="opacity-75">
-          Setting
+        <Link href="/profile" className="inline-block">
+          Your Profile
         </Link>
-
-        <div className="m-auto">
-          {status === "authenticated" ? (
-            <div>
-              <img className="rounded w-8" src={session.user.image} onClick={toggleOpen} />
-              {open && <ProfileOptions />}
-              <p className="text-white text-xs">{session.user.name}</p>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
       </div>
     </div>
   );
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import Login from "../../components/Login";
+import SignOut from "../../components/SignOut";
 import { ProfileLayout } from "../../layouts/ProfileLayout";
 
 function ProfilePage() {
@@ -10,7 +11,28 @@ function ProfilePage() {
     },
   });
 
-  return <ProfileLayout pageTitle={"Profile"} />;
+  return (
+    <ProfileLayout pageTitle="Profile">
+      <div className="grid grid-cols-2 gap-x-10 my-10 h-(msngr-height)">
+        <div className="border rounded bg-gray-200 relative p-4">
+          <h3 className="text-xl font-bold mb-4">Settings </h3>
+          <div className="absolute top-5 right-5">
+            <SignOut />
+          </div>
+          <div>
+            <img className="rounded w-32" src={session.user.image} />
+            <p className="">
+              <b>Your name</b> : {session.user.name}
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="border rounded bg-gray-200 h-2/5 mb-6"></div>
+          <div className="border rounded bg-gray-200 h-2/5"></div>
+        </div>
+      </div>
+    </ProfileLayout>
+  );
 }
 
 ProfilePage.auth = {
