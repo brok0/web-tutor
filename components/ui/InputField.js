d6b57f430aeb27a3228e071aae4352fc67b4bb04
brok0 <bglinskii6@gmail.com>
2022-08-04 16:20:23 +0300

    Update InputField.js

--- components/ui/InputField.js
+++ components/ui/InputField.js
@@ -1,27 +1,24 @@
 import { Field, ErrorMessage } from "formik";
+import { memo } from "react";
 
-export const InputField = ({ label, name, type, subtype, options }) => {
-	return (
-		<div className="mx-2 my-1 w3/4 flex flex-col">
-			<label className="font-bolder">{label}</label>
-			{!options ? (
-				<Field as={subtype} type={type} name={name} className="border px-1" />
-			) : (
-				<Field as="select" name={name} className="border">
-					<option value=""></option>
-					{options.map((el) => (
-						<option value={el} key={el}>
-							{el}
-						</option>
-					))}
-				</Field>
-			)}
+export const InputField = memo(({ label, name, type, subtype, options }) => {
+  return (
+    <div className="mx-2 my-1 w3/4 flex flex-col">
+      <label className="font-bolder">{label}</label>
+      {!options ? (
+        <Field as={subtype} type={type} name={name} className="border px-1" />
+      ) : (
+        <Field as="select" name={name} className="border">
+          <option value=""></option>
+          {options.map((el) => (
+            <option value={el} key={el}>
+              {el}
+            </option>
+          ))}
+        </Field>
+      )}
 
-			<ErrorMessage
-				name={name}
-				component="div"
-				className="text-sm text-red-600 "
-			/>
-		</div>
-	);
-};
+      <ErrorMessage name={name} component="div" className="text-sm text-red-600 " />
+    </div>
+  );
+});
